8c6036645ddedb109efc6c031a427706146c6b4a
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-06-30 16:49:31 +0700

    add tab top 5 shopitm

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -1,10 +1,12 @@
 <script>
   import { onMount } from "svelte";
-  let tabs = ["อันดับ #1", "อันดับ #2", "อันดับ #3"];
-  let activeTabIndex = 0;
+  let tabs = ["อันดับ #1", "ไอเทมยอดนิยม", "อันดับ #3"];
+  let activeTabIndex = 1;
 
   //
   let data = null;
+  let buymost = null;
+
   const LIMIT = 5;
 
   async function fetchData() {
@@ -23,8 +25,23 @@
     }
   }
 
+  async function shoptop() {
+    try {
+      const response = await fetch("http://127.0.0.1:3005/shoptop");
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      const responseData = await response.json();
+      buymost = responseData.slice(0, LIMIT);
+      // data = await response.json(); //showall
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   // Call the fetch function when the component is mounted
   onMount(fetchData);
+  onMount(shoptop);
   //
 
   console.log(
@@ -98,25 +115,65 @@
           </tr>
         {/each}
       {/if}
-
-      <!-- <tr>
-          <th><i class="fa-solid fa-xl fa-trophy text-zinc-300"></th>
-          <td>Hart Hagerty</td>
-          <td>Desktop Support Technician</td>
-        </tr>
-
-        <tr>
-          <th><i class="fa-solid fa-xl fa-trophy text-amber-600"></i></th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-        </tr> -->
     </tbody>
   </table>
 {/if}
 
 {#if activeTabIndex == 1}
-  <div>content 2</div>
-{/if}
+  <table class="table">
+    <thead>
+      <tr>
+        <th />
+        <th>ไอเทม</th>
+        <th>จำนวน</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- row loop -->
+      {#if buymost !== null}
+        {#if buymost.length > 0}
+          {#each buymost as item, index}
+            <tr>
+              {#if index === 0}
+                <th><i class="fa-solid fa-xl fa-trophy text-amber-400" /></th>
+              {:else if index === 1}
+                <th><i class="fa-solid fa-xl fa-trophy text-zinc-300" /></th>
+              {:else if index === 2}
+                <th><i class="fa-solid fa-xl fa-trophy text-amber-600" /></th>
+              {:else}
+                <th />
+              {/if}
+
+              <td>{item.name}</td>
+              <td>{item.buycount}</td>
+            </tr>
+          {/each}
+        {:else}
+          <p>ไม่มีข้อมูล.</p>
+        {/if}
+      {:else}
+        {#each Array(5) as _, i}
+          <tr class="animate-pulse">
+            {#if i === 0}
+              <th><i class="fa-solid fa-xl fa-trophy text-amber-400" /></th>
+            {:else if i === 1}
+              <th><i class="fa-solid fa-xl fa-trophy text-zinc-300" /></th>
+            {:else if i === 2}
+              <th><i class="fa-solid fa-xl fa-trophy text-amber-600" /></th>
+            {:else}
+              <th />
+            {/if}
+            <td>
+              <div class="h-4 bg-slate-700 rounded" />
+            </td>
+            <td>
+              <div class="h-3 bg-slate-700 rounded" />
+            </td>
+          </tr>
+        {/each}
+      {/if}
+    </tbody>
+  </table>{/if}
 
 {#if activeTabIndex == 2}
   <div>content 3</div>
