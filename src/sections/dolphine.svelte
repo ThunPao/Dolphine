d86f0b5416f034b314532287288a19a6eb769a20
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-07-04 11:55:28 +0700

    Update dolphine.svelte

--- src/sections/dolphine.svelte
+++ src/sections/dolphine.svelte
@@ -1,24 +1,10 @@
 <script>
   import { onMount } from "svelte";
-  import { dataset_dev } from "svelte/internal";
 
   let sub = false;
   let cooldown = false;
-  let heart = false;
 
   var url = "http://127.0.0.1:3005/";
-
-  function toggleheart(id) {
-    console.log("Toggled + " + id);
-    if (!cooldown) {
-      heart = !heart;
-
-      cooldown = true;
-      setTimeout(() => {
-        cooldown = false;
-      }, 500); // 0.5 seconds cooldown
-    }
-  }
 
   function togglesub() {
     if (!cooldown) {
@@ -30,8 +16,10 @@
     }
   }
 
+  let playerinfo = null;
   let blogs = null;
   let blogdata = null;
+  let blogdata_p = null;
 
   async function getblogs() {
     try {
@@ -42,20 +30,49 @@
       }
       const responseData = await response.json();
       blogs = responseData.slice(0, 5);
+      // blogs = responseData;
+
       // data = await response.json(); //showall
     } catch (error) {
       console.error(error);
     }
   }
 
-  async function getblogdatas() {
+  async function getblogdataplayer() {
     try {
-      const response = await fetch("http://127.0.0.1:3005/blogdata/1");
+      const response = await fetch(url + "blogdata/1");
       if (!response.ok) {
         throw new Error("Request failed");
       }
       const responseData = await response.json();
-      blogdata = responseData.slice(0, 5);
+      blogdata_p = responseData.slice(0, 5);
+      // data = await response.json(); //showall
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getplayers() {
+    try {
+      const response = await fetch(url + "player/2");
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      const responseData = await response.json();
+      playerinfo = responseData;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getblogdata() {
+    try {
+      const response = await fetch(url + "blogdata");
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      const responseData = await response.json();
+      blogdata = responseData;
       // data = await response.json(); //showall
     } catch (error) {
       console.error(error);
@@ -65,7 +82,7 @@
   async function toggleFavorite(playerId, blogId) {
     try {
       // Make a POST request to the server
-      const response = await fetch("http://localhost:3005/blogdata", {
+      const response = await fetch(url + "blogdata", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -75,7 +92,9 @@
 
       // Check the response status
       if (response.ok) {
-        getblogdatas();
+        getblogdata();
+        getblogdataplayer();
+
         // If the request is successful, update the local blogdata array
       } else {
         // If the request fails, display an error message
@@ -86,8 +105,10 @@
     }
   }
 
+  onMount(getplayers);
   onMount(getblogs);
-  onMount(getblogdatas);
+  onMount(getblogdataplayer);
+  onMount(getblogdata);
 </script>
 
 <div class="card w-full bg-base-100 shadow-xl px-2">
@@ -157,19 +178,30 @@
             />
           </button>
           <div class="flex justify-end mr-3 p-3">
-            <!-- <b>{data.likes}</b>&nbsp Liked &nbsp -->
+            {#if playerinfo !== null && playerinfo.length > 0}
+              {playerinfo.filter((item) => item.p_role === 1)
+                ? "MEMBER"
+                : "VIP"}
+              {#each playerinfo as pinfo}
+                {pinfo.name}
+              {/each}
+            {/if}
 
-            {#if blogdata !== null && blogdata.length > 0}
-              {blogdata.filter((item) => item.blogliked.includes(data.id))
-                .length}
+            {#if blogdata_p !== null && blogdata_p.length > 0}
+              <!-- blogdata -->
+              {#if blogdata !== null && blogdata.length > 0}
+                {blogdata.filter((item) => item.blogliked.includes(data.id))
+                  .length} Likes &nbsp
+              {/if}
+
               <button on:click={toggleFavorite(1, data.id)} class="text-2xl">
                 <i
-                  class="{blogdata.find((item) =>
+                  class="{blogdata_p.find((item) =>
                     item.blogliked.includes(data.id)
                   )
                     ? 'fa-solid'
                     : 'fa-regular'} fa-heart active:animate-ping"
-                  style={blogdata.find((item) =>
+                  style={blogdata_p.find((item) =>
                     item.blogliked.includes(data.id)
                   )
                     ? "color: #ff0000;"
