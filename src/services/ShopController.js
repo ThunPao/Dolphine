1f3867bf104ff21328e9c82fccd13ee0822b59a3
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-07-11 10:57:50 +0700

    Update ShopController.js

--- src/services/ShopController.js
+++ src/services/ShopController.js
@@ -1,4 +1,6 @@
 import toastr from "toastr";
+import { currentuser } from "../services/Authen";
+
 async function handleclick() {
     // Display an info toast with no title
     toastr.success(
@@ -17,11 +19,12 @@ async function handleclick() {
     console.log("Delayed log after 0.5 seconds");
   }
 
-  export async function buyitem(did) {
+  export function buyitem(did) {
+    console.log("Buyed "+did);
     // Display an info toast with no title
     toastr.success(
       "Success!",
-      "ซื้อสินค้า " + title + " สำเร็จ ยอดเงินคงเหลือ " + currentuser?.points,
+      "ซื้อสินค้า title "+ " สำเร็จ ยอดเงินคงเหลือ " + currentuser?.points,
       {
         timeOut: 5000,
         positionClass: "toast-bottom-center",
@@ -29,8 +32,4 @@ async function handleclick() {
         progressBar: true,
       }
     );
-
-    await new Promise((resolve) => setTimeout(resolve, 500));
-
-    console.log("Delayed log after 0.5 seconds");
   }
