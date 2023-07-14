0cd0c158df30c7625711e0f52d13d963d8b9c838
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-07-14 16:11:06 +0700

    implement Client only

--- src/sections/cards.svelte
+++ src/sections/cards.svelte
@@ -12,29 +12,29 @@
 
   onMount(async () => {
     await tick(); // Wait for the DOM to be fully updated
-    getshopitems();
   });
 
   afterUpdate(() => {
-    if ($shopitems !== null && $shopitems.length > 0) {
-      slider = new KeenSlider("#shopitem-slides", {
-        breakpoints: {
-          "(min-width: 400px)": {
-            slides: { perView: 2, spacing: 5 },
-          },
-          "(min-width: 768px)": {
-            slides: { perView: 3, spacing: 10 },
-          },
-          "(min-width: 1000px)": {
-            slides: { perView: 4, spacing: 10 },
-          },
+    // if ($shopitems !== null && $shopitems.length > 0) {
+    slider = new KeenSlider("#shopitem-slides", {
+      breakpoints: {
+        "(min-width: 400px)": {
+          slides: { perView: 2, spacing: 5 },
         },
-        slides: {
-          perView: 1,
-          spacing: 15,
+        "(min-width: 768px)": {
+          slides: { perView: 3, spacing: 10 },
         },
-      });
-    }
+        "(min-width: 1000px)": {
+          slides: { perView: 4, spacing: 10 },
+        },
+      },
+      slides: {
+        perView: 1,
+        spacing: 15,
+      },
+    });
+    // }
+    getshopitems();
   });
 </script>
 
