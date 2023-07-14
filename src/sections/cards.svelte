bb9e4d52ed17b5089dd4f987b4e70a8cee5e466d
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-07-14 15:47:18 +0700

    Update keenslider shop card problem
    fix problem slider of keenslider function won't load by adding afterUpdate

--- src/sections/cards.svelte
+++ src/sections/cards.svelte
@@ -1,95 +1,87 @@
 <script>
-  import { onMount } from "svelte";
-  const cardimg = 'https://via.placeholder.com/300x250'
-  import Shopcard from '../components/Shopitem.svelte';
-  import {shopitems,getshopitems} from '../services/ShopController';
-  import Tabs from '../components/Tabs.svelte';
+  const cardimg = "https://via.placeholder.com/300x250";
+  import Shopcard from "../components/Shopitem.svelte";
+  import { shopitems, getshopitems } from "../services/ShopController";
+  import Tabs from "../components/Tabs.svelte";
+  // import "../services/Sliders";
+  import { onMount, tick, afterUpdate } from "svelte";
+  import KeenSlider from "keen-slider";
+  import "keen-slider/keen-slider.min.css";
 
-  import KeenSlider from 'keen-slider';
-  import 'keen-slider/keen-slider.min.css';
+  let slider;
 
-  // const response = await fetch('http://127.0.0.1:3005/shopitems');
-  // const shopitems = await response.json();
-  
-  // const shopitems = await fetch(apiurl+'shopitems').then(r => r.json());
-
-
-  onMount(() => {
-    // document.createElement('div');
-  getshopitems();
-  const slider = new KeenSlider("#shopitem-slides", {
-    breakpoints: {
-      "(min-width: 400px)": {
-        slides: { perView: 2, spacing: 5 },
-      },
-      "(min-width: 768px)": {
-        slides: { perView: 3, spacing: 10 },
-      },
-      "(min-width: 1000px)": {
-        slides: { perView: 4, spacing: 10 },
-      },
-    },
-    slides: {
-      perView: 1, 
-      spacing: 15,
-    },
+  onMount(async () => {
+    await tick(); // Wait for the DOM to be fully updated
+    getshopitems();
   });
-});
 
-  </script>
-
+  afterUpdate(() => {
+    if ($shopitems !== null && $shopitems.length > 0) {
+      slider = new KeenSlider("#shopitem-slides", {
+        breakpoints: {
+          "(min-width: 400px)": {
+            slides: { perView: 2, spacing: 5 },
+          },
+          "(min-width: 768px)": {
+            slides: { perView: 3, spacing: 10 },
+          },
+          "(min-width: 1000px)": {
+            slides: { perView: 4, spacing: 10 },
+          },
+        },
+        slides: {
+          perView: 1,
+          spacing: 15,
+        },
+      });
+    }
+  });
+</script>
 
 <div class="grid grid-cols-1 xl:grid-cols-4 gap-3 m-5">
+  <div class="carditems md:col-span-3">
+    <!--  -->
+    <div class="">
+      <!-- <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" > -->
 
-<div class="carditems md:col-span-3" >
-<!--  -->
-<div class="">
-    <!-- <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" > -->
+      <!-- Slider main container -->
+      <div id="shopitem-slides" class="keen-slider">
+        <!-- Additional required wrapper -->
 
-<!-- Slider main container -->
-<div id="shopitem-slides" class="keen-slider">
-  <!-- Additional required wrapper -->
+        <!-- Slides -->
+        <!-- {shopitems.map((data: any) => -->
+        {#if $shopitems !== null && $shopitems.length > 0}
+          {#each $shopitems as data}
+            <Shopcard
+              id={data.id}
+              title={data.name}
+              href={cardimg}
+              buycount={data.buycount}
+              point={data.point}
+              description={data.description}
+            />
+          {/each}
+        {/if}
 
-    <!-- Slides -->
-    <!-- {shopitems.map((data: any) => -->
-
-      {#if $shopitems !== null && $shopitems.length > 0}
-      {#each $shopitems as data}
-      <Shopcard
-      id={data.id}
-       title={data.name}
-       href={cardimg}
-       buycount={data.buycount}
-      point={data.point}
-       description={data.description}
-       />
-       {/each}
-       {/if}
-    <!-- )} -->
-
-</div>
-        <!-- </div> -->
-</div>
-</div>
-<div class="side-carditems bg-base-200 rounded-lg">
-    <div class="overflow-x-auto">
-      <Tabs/>
+        <!-- )} -->
       </div>
+      <!-- </div> -->
+    </div>
+  </div>
+  <div class="side-carditems bg-base-200 rounded-lg">
+    <div class="overflow-x-auto">
+      <Tabs />
+    </div>
+  </div>
 </div>
-
-</div>
-
 
 <div class="bg-base-200 grid gap-2 p-3 justify-center">
-  <div class="text-2xl font-medium">
-    รายการไอเทมทั้งหมด
-    
-  </div>
+  <div class="text-2xl font-medium">รายการไอเทมทั้งหมด</div>
 
   <div class="join">
     <div>
       <div>
-        <input class="input input-bordered join-item" placeholder="Search..."/>
+        <input class="input input-bordered join-item" placeholder="Search..." />
       </div>
     </div>
     <select class="select select-bordered join-item">
@@ -98,28 +90,23 @@
       <option>Drama</option>
       <option>Action</option>
     </select>
-
-  </div>
-
-
-
-  </div>
-
-  <div class="grid gap-2 p-1">
-    {#if $shopitems !== null && $shopitems.length > 0}
-    {#each $shopitems as data}
-<div class="card card-side bg-base-100 shadow-xl">
-  <figure><img src={cardimg} alt="Movie"/></figure>
-  <div class="card-body">
-    <h2 class="card-title">{data.name}</h2>
-    <p>{data.point}</p>
-
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">ซื้อไอเทม</button>
-    </div>
   </div>
 </div>
 
-{/each}
-{/if}
+<div class="grid gap-2 p-1">
+  {#if $shopitems !== null && $shopitems.length > 0}
+    {#each $shopitems as data}
+      <div class="card card-side bg-base-100 shadow-xl">
+        <figure><img src={cardimg} alt="Movie" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">{data.name}</h2>
+          <p>{data.point}</p>
+
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">ซื้อไอเทม</button>
+          </div>
+        </div>
+      </div>
+    {/each}
+  {/if}
 </div>
---
+++ src/services/Sliders.js
@@ -0,0 +1,22 @@
+import KeenSlider from "keen-slider";
+import "keen-slider/keen-slider.min.css";
+
+
+  // Initialize the slider
+  let slider = new KeenSlider("#shopitem-slides", {
+    breakpoints: {
+      "(min-width: 400px)": {
+        slides: { perView: 2, spacing: 5 },
+      },
+      "(min-width: 768px)": {
+        slides: { perView: 3, spacing: 10 },
+      },
+      "(min-width: 1000px)": {
+        slides: { perView: 4, spacing: 10 },
+      },
+    },
+    slides: {
+      perView: 1,
+      spacing: 15,
+    },
+  });
