e50187a38d9bd1b96307975c972658f48e6a61f7
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-07-31 22:24:47 +0700

    Update apiurl.js

--- src/services/apiurl.js
+++ src/services/apiurl.js
@@ -1,6 +1,6 @@
-let svless = 0;
+let svless = 1;
 export var apiurl = "http://127.0.0.1:3005/";
 
 if (svless > 0){
-apiurl = "https://dolphine-feeder.vercel.app/";
+apiurl = "https://dolphine-fastify.vercel.app/";
 }
