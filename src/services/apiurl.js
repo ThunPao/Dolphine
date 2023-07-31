64df19ffa2f9780d25d7052eb45a87d62e8e6016
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-07-31 23:52:26 +0700

    Update apiurl.js

--- src/services/apiurl.js
+++ src/services/apiurl.js
@@ -1,6 +1,9 @@
-let svless = 1;
+let svless = 2;
 export var apiurl = "http://127.0.0.1:3005/";
 
-if (svless > 0){
+if (svless == 1){
 apiurl = "https://dolphine-fastify.vercel.app/";
+}
+if(svless ==2){
+    apiurl = "http://dolphine.servegame.com:3005/"
 }
