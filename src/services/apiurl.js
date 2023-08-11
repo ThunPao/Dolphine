8809188b74201ab9db9d83444898caab5623629a
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-08-11 15:59:18 +0700

    update gitignore

---
+++ src/services/apiurl.js
@@ -0,0 +1,12 @@
+let svless = 0;
+export var apiurl = "http://127.0.0.1:3005/";
+
+if (svless == 1){
+apiurl = "https://dolphine-fastify.vercel.app/";
+}
+if(svless == 2){
+    apiurl = "http://dolphine.servegame.com:3005/"
+}
+export var imgurl = apiurl+"images/shopitems/"
+
+//sv
