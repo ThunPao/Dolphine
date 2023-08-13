dbbbd75295d5ba3c298e759bc5dc774aa93667d3
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-08-13 15:40:23 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import vercel from '@astrojs/vercel/serverless';
-// import vercel from '@astrojs/vercel/edge';
 import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
@@ -29,7 +28,7 @@ export default defineConfig({
   //   mode: "standalone"
   // }),
 
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel({
     analytics: true
   }),
