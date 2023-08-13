908425530dbbcef8e3dfbbbb956f35f619b2cd9b
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-08-13 15:09:38 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-// import vercel from '@astrojs/vercel/serverless';
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/edge';
 import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
