260e696c01faf73bdd6a2c8bbc6e04061de55027
ThunPao <52374829+ThunPao@users.noreply.github.com>
2023-08-16 09:12:55 +0700

    Update apiurl.js

--- src/services/apiurl.js
+++ src/services/apiurl.js
@@ -1,4 +1,4 @@
-let svless = 1;
+let svless = 3;
 export var apiurl = "http://127.0.0.1:3005/";
 
 if (svless == 1){
@@ -8,7 +8,7 @@ if(svless == 2){
     apiurl = "http://dolphine.servegame.com:3005/"
 }
 if(svless == 3){
-    apiurl = "https://www.dolphine.online/"
+    apiurl = "https://fish.dolphine.online/"
 }
 export var imgurl = apiurl+"images/shopitems/"
 
